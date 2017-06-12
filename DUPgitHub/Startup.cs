bfe9a3aee049a966954a75c65757aa646305f20e
10449953 <10449953@INFO4430-DUP>
2017-06-11 19:27:23 -0600

    Add project files.

---
+++ DUPgitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DUPgitHub.Startup))]
+namespace DUPgitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
